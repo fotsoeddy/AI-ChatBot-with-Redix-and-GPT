2fdf88ff945c88b8cff58503d139c8c81f5aabf0
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2025-03-15 22:21:19 +0100

    we will define the class that controls the connections to our WebSockets

--- server/src/routes/chat.py
+++ server/src/routes/chat.py
@@ -18,3 +18,19 @@ async def token_generator(name: str, request: Request):
     data = {"name": name, "token": token}
 
     return data
+
+from ..socket.connection import ConnectionManager
+
+manager = ConnectionManager()
+
+@chat.websocket("/chat")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            print(data)
+            await manager.send_personal_message(f"Response: Simulating response from the GPT service", websocket)
+
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
